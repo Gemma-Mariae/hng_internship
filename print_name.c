b11a41fc159245b661a5f792c9c29c857508966e
Ukonu Divine Chisom <divinechisom1995@gmail.com>
2021-08-19 11:25:34 +0100

    adding print_name file

---
+++ print_name.c
@@ -0,0 +1,18 @@
+/*
+ * File: print_name.c
+ * Author: Ukonu, Divine Chisom
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints my name.
+ * Return: zero.
+ */
+
+int main(void)
+{
+    char name[] = "Ukonu Divine Chisom";
+    printf(name);
+    return 0;
+}
